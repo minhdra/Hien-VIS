7f11e0f22f64b3268c516e05a8b7448c94159dd2
Le Minh <le.minh011100@gmail.com>
2022-10-19 11:18:31 +0700

    remove view engine

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const connection = mongoose.connection;
 connection.once('open', () => console.log('connection successfully'));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
