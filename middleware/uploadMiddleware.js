7a23df93271af7d80327f7bc5de1e8303adfc146
Le Minh <le.minh011100@gmail.com>
2022-10-27 11:02:19 +0700

    fixed upload middleware

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -6,20 +6,27 @@ const multer = require('multer');
 // SET STORAGE
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/images")
+    cb(null, './public/images');
   },
   filename: function (req, file, cb) {
-    const listName = file.originalname.split('.');
-    const type = listName[listName.length - 1];
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + type)
-  }
-})
+    let ext = '';
+    switch (file.mimetype) {
+      case 'image/jpeg':
+        ext = '.jpeg';
+        break;
+      case 'image/png':
+        ext = '.png';
+        break;
+    }
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + ext);
+  },
+});
 
 const upload = multer({
-  limits: {
-    fileSize: 4 * 1024 * 1024,
-  },
+  // limits: {
+  //   fileSize: 4 * 1024 * 1024,
+  // },
   // dest: "public/images",
   storage: storage,
 });
