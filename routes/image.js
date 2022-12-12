d87620dc1ae232b279d3c1c2e825e53d62720fad
Le Minh <le.minh011100@gmail.com>
2022-12-12 13:03:08 +0700

    updated 12/12

--- routes/image.js
+++ routes/image.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var ImageController = require('../controllers/ImageController');
-var upload = require('../middleware/uploadMiddleware');
+// var upload = require('../middleware/uploadMiddleware');
 var fileUploader = require('../configs/cloudinary.config');
 var verifyToken = require('../middleware/verifyToken');
 
 /* GET users listing. */
-router.post('/upload', verifyToken, upload.single('file'), ImageController.uploadSingle);
+// router.post('/upload', verifyToken, upload.single('file'), ImageController.uploadSingle);
 router.post('/cloudinary-upload', verifyToken, fileUploader.single('file'), ImageController.uploadSingle);
 // router.post('/remove', verifyToken, ImageController.remove);
 
